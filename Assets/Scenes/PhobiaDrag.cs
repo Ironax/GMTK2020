30599b2edd2932952895381a2529c1d482f25cfd
Ironax <matthieu.ronarch@gmail.com>
2020-07-11 02:18:36 +0200

    Drag and drop proto

---
+++ Assets/Scenes/PhobiaDrag.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+public class PhobiaDrag : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+	[SerializeField] private GameObject phobiaPrefab = null;
+	private Canvas canvas = default;
+	private RectTransform rectTransform = default;
+	private Vector2 startPos = default;
+
+    void Start()
+	{
+		canvas = GetComponentInParent<Canvas>();
+		rectTransform = GetComponent<RectTransform>();
+	}
+
+    void Update()
+    {
+        
+    }
+
+	public void OnPointerDown(PointerEventData eventData)
+	{ }
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		startPos = rectTransform.anchoredPosition;
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePosWorld.z = 0;
+		Instantiate(phobiaPrefab, mousePosWorld, Quaternion.identity);
+		rectTransform.anchoredPosition = startPos;
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+	}
+}
