69944b37ecf4bd353b01909218916712957f9683
Alice DOUSSIN <a.doussin@student.isartdigital.com>
2020-07-12 15:31:20 +0200

    menu buttons and credits updated

---
+++ Assets/Scripts/DeleteSave.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeleteSave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(DeletePlayerPrefs);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+}
