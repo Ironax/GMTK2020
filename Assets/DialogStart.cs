3229cc716f663fc931ca0c8eb67bd8377799898c
Ironax <matthieu.ronarch@gmail.com>
2020-07-12 15:33:59 +0200

    Dialogs

---
+++ Assets/DialogStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogStart : DialogTrigger
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogBox.StartDialog(dialog);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/DialogTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DialogTrigger : MonoBehaviour
+{
+	[SerializeField] protected Dialog dialog;
+
+	protected DialogBox dialogBox;
+    // Start is called before the first frame update
+    void Awake()
+	{
+		dialogBox = FindObjectOfType<DialogBox>();
+	}
+
+	protected void TriggerDialog()
+	{
+		dialogBox.StartDialog(dialog);
+	}
+}
