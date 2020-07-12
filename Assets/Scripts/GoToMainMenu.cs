305ac35d6a5d0e1b82e83831dbd9b51f5f2b17e4
Alice DOUSSIN <a.doussin@student.isartdigital.com>
2020-07-12 15:03:02 +0200

    add music, anim and home button

---
+++ Assets/Scripts/GoToMainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GoToMainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(LoadMainMenu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+
+        SceneManager.LoadScene("Main Menu");
+
+    }
+}
